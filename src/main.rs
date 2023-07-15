65dfc066bdf6d8c4a978911fa39f25494f77f9e4
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2023-07-15 22:07:53 +0200

    now takes char_id as cml argument

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,22 @@
 
 mod player;
 mod fetch;
+mod ui;
 use player::PlayerData;
+use std::env;
 
-static P_ID: &str = "48486396";
 
 #[tokio::main]
 async fn main() {
+    let mut P_ID: String = "48486396".to_owned();
+    //Until UI is finished
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Too many or no arguments given. Using default char_id")
+    } else {
+        P_ID = args[1].clone();
+    }
+
     //Initial player data
     let mut P_DATA: PlayerData = PlayerData {
         name: String::new(),
@@ -20,9 +30,9 @@ async fn main() {
         server: String::new(),
         achievements: Vec::new()
     };
-
+    
     //fetch data from api and do provisional print
-    let _ = fetch::fetch_data(P_ID, &mut P_DATA).await;
+    let _ = fetch::fetch_data(&P_ID, &mut P_DATA).await;
     println!("Char: {}\nLevel: {}\nClass: {}\nDC: {}\nServer: {}\nAchievements: {:?}",
              P_DATA.name,
              P_DATA.level,
@@ -31,4 +41,7 @@ async fn main() {
              P_DATA.server,
              P_DATA.achievements
             );
+
+    //For now we create the ui after we fetched the data
+    let gui = ui::create_ui();
 }
