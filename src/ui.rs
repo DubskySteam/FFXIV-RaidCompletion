1e490d75faee44727ed80431d2c20f047c85fda4
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2023-07-15 22:23:23 +0200

    update default win_size: 720x1080 (bugged?)

--- src/ui.rs
+++ src/ui.rs
@@ -2,7 +2,12 @@ use dioxus::prelude::*;
 use dioxus_desktop::*;
 
 pub fn create_ui() {
-    dioxus_desktop::launch(App);
+    dioxus_desktop::launch_cfg(
+        App,
+        Config::default().with_window(WindowBuilder::new()
+                                      .with_resizable(false)
+                                      .with_inner_size(dioxus_desktop::wry::application::dpi::LogicalSize::new(1080, 720)))
+        );
 }
 
 fn App(cx: Scope) -> Element {
