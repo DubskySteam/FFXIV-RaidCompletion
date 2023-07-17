fc39ef3fd2f3c33b1b11aa5569c4e5ad226d3949
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2023-07-17 03:46:39 +0200

    added content management struct

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # FFXIV-RaidCompletion
 ##### A fast and lightweight tracker for dungeon, trial & raid achievements
 ##### Made & maintained in my free-time, therefore no guarantee for instant bug fixes, updates etc.
-![](https://img.shields.io/badge/Dev%20Version-0.5-red?style=for-the-badge&logo=git)
+![](https://img.shields.io/badge/Current%20Version-0.5-red?style=for-the-badge&logo=git)
 ![](https://img.shields.io/badge/Game%20Version-6.4-blue?style=for-the-badge&logo=)
 ___
-[![](https://img.shields.io/badge/Download%20latest-v0.4-blue?style=for-the-badge&logo=)](https://github.com/DubskySteam/FFXIV-RaidCompletion/releases/)
+[![](https://img.shields.io/badge/Download%20latest-v0.5-blue?style=for-the-badge&logo=)](https://github.com/DubskySteam/FFXIV-RaidCompletion/releases/)
 ## Features [WIP]
 - Overview of your character
 - Show progression of pve content
--- src/content.rs
+++ src/content.rs
@@ -1,18 +1,24 @@
-pub fn getDungeons() -> Vec<String> {
-    let x = vec!["Shiva".to_owned(), "Whorleater".to_owned()];
-    x
+pub struct achievements {
+    pub name: Vec<String>,
+    pub id: Vec<i32>,
+    pub status: Vec<bool>
 }
 
-pub fn getTrials() -> Vec<String> {
-    let x = vec![
-        "Ultima's bane".to_owned(),
-        "Howling Eye".to_owned(),
-        "Navel".to_owned(),
-        "Bowl of Embers".to_owned(),
-        "Thornmarch".to_owned(),
-        "Whorleater".to_owned(),
-        "Striking Tree".to_owned(),
-        "Akh Afah Amphithreatre".to_owned(),
-    ];
-    x
+impl achievements {
+    pub fn populateTrials() -> Self {
+        println!("populateTrials");
+        achievements {
+            name: vec![
+                "Howling Eye".to_owned(),
+                "Navel".to_owned(),
+                "Bowl of Embers".to_owned(),
+                "Thornmarch".to_owned(),
+                "Whorleater".to_owned(),
+                "Striking Tree".to_owned(),
+                "Akh Afah Amphithreatre".to_owned(),
+            ],
+            id: vec![856, 857, 855, 894, 893, 994, 1045],
+            status: vec![false, false, false, false, false, false, false]
+        } 
+    }
 }
